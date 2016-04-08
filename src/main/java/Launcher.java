25b096e29050c510b85bf94879edd3e5ab883c2a
Jim <jim@get-e.com>
2016-04-08 11:41:38 +0200

    added pom, added base for startscreen

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Jim on 4/2/2016.
+ *
+ * @author Jim
+ * @version 1.0
+ * @since 4/2/2016
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("startscreen.fxml"));
+        primaryStage.setTitle("ZombieSkoro");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/startscreenController.java
@@ -0,0 +1,24 @@
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Jim on 4/7/2016.
+ *
+ * @author Jim
+ * @version 1.0
+ * @since 4/7/2016
+ */
+public class startscreenController implements Initializable {
+
+
+    public GridPane root;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
