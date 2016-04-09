8774528761e3edc38703dd41f4c0a96b546cb60c
Jim <jim@get-e.com>
2016-04-09 16:03:11 +0200

    Added overlays

--- src/main/java/startscreenController.java
+++ src/main/java/startscreenController.java
@@ -2,6 +2,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,10 +23,34 @@ public class startscreenController implements Initializable {
     public Button helpButton;
     public Button highscoresButton;
     public Button exitButton;
+    public VBox highscoreOverlay;
+    public VBox instructionsOverlay;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        addEvents();
+
+    }
+
+    private void addEvents() {
+        //StartButton -> starts game
+
+        //Instructions -> overlay with instructions
+        helpButton.setOnAction(event -> instructionsOverlay.setVisible(true));
+        instructionsOverlay.setOnMouseClicked(event -> instructionsOverlay.setVisible(false));
+
+        //Highscores -> overlay with highscores
+        highscoresButton.setOnAction(event -> highscoreOverlay.setVisible(!highscoreOverlay.isVisible()));
+        root.setOnMouseClicked(event -> {
+            highscoreOverlay.setVisible(false);
+            instructionsOverlay.setVisible(false);
+        });
+        highscoreOverlay.setOnMouseClicked(event -> highscoreOverlay.setVisible(false));
+
+        exitButton.setOnAction(event -> {
+            System.exit(0);
+        });
     }
 
 }
